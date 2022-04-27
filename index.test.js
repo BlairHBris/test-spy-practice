fe3ec5bb0e582353dcb34d5d49a1f61346b1053f
Blair Bristow <blairhbristow@gmail.com>
2022-04-27 15:28:01 -0400

    Rename variables

--- index.test.js
+++ index.test.js
@@ -12,25 +12,25 @@ function map(array, transform) {
 
 describe("Tests for the map function", () => {
     it("Returns a transformed array of items when given a single item", () => {
-        const singleItemArray = [1];
+        const array = [1];
         const transform = jest.fn(() => 2);
 
-        const transformedArray = map(singleItemArray, transform);
+        const newArray = map(array, transform);
 
-        expect(transformedArray).toEqual([2]);
+        expect(newArray).toEqual([2]);
     });
 
     it("Returns a transformed array of items when given multiple items", () => {
-        const multiItemArray = [1, 2, 3];
+        const array = [1, 2, 3];
         const transform = jest
             .fn()
             .mockReturnValueOnce(2)
             .mockReturnValueOnce(4)
             .mockReturnValueOnce(6);
 
-        const transformedArray = map(multiItemArray, transform);
+        const newArray = map(array, transform);
 
-        expect(transformedArray).toEqual([2, 4, 6]);
+        expect(newArray).toEqual([2, 4, 6]);
     });
 })
 
@@ -50,19 +50,19 @@ function filter(array, predicate) {
 
 describe("Tests for the filter function", () => {
     it("Returns only even numbers when given multiple numbers and an isEven function", () => {
-        const numberArray = [1, 2, 3];
+        const array = [1, 2, 3];
         const isEven = jest
             .fn()
             .mockReturnValueOnce(false)
             .mockReturnValueOnce(true)
             .mockReturnValueOnce(false);
 
-        const filteredArray = filter(numberArray, isEven);
+        const newArray = filter(array, isEven);
 
-        expect(filteredArray).toEqual([2]);
+        expect(newArray).toEqual([2]);
     });
     it("Returns only active users  when given an array of users and an isActive function", () => {
-        const userArray = [
+        const array = [
             {
                 name: "Kyle",
                 isActive: true
@@ -77,9 +77,9 @@ describe("Tests for the filter function", () => {
             .mockReturnValueOnce(true)
             .mockReturnValueOnce(false);
 
-        const filteredArray = filter(userArray, isActive);
+        const newArray = filter(array, isActive);
 
-        expect(filteredArray).toEqual([
+        expect(newArray).toEqual([
             {
                 name: "Kyle",
                 isActive: true
@@ -87,7 +87,3 @@ describe("Tests for the filter function", () => {
         ]);
     });
 })
-
-
-
-
